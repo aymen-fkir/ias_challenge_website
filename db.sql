7dc3bb5ad2adb46c66b484a5817f8db6aa139213
Aymen <aymenfkir23@gmail.com>
2023-12-05 21:32:46 +0100

    IAS challenge  WEBSITE

--- db.sql
+++ db.sql
@@ -1,1 +1,24 @@
-SELECT * FROM robot_data where 
+CREATE TABLE robot_data (
+    id INT PRIMARY KEY,
+    project_id INT,
+    robot_id INT,
+    robot_state VARCHAR(255),
+    energy_consumption INT,
+    robot_condition VARCHAR(255) 
+);
+
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    project_id INT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    FOREIGN KEY (project_id) REFERENCES robot_data(project_id)
+);
+CREATE TABLE weather_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    weather_condition VARCHAR(255) NOT NULL,
+    air_condition VARCHAR(255) NOT NULL,
+    project_id INT,
+    FOREIGN KEY (project_id) REFERENCES users(project_id)
+);
